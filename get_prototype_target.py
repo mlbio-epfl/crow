a2d02c4d4b5dfddef944825b77e4f7a5854cb69f
wenshuo128 <45825364+wenshuo128@users.noreply.github.com>
2024-11-15 12:10:54 +0100

    Update get_prototype_target.py

--- get_prototype_target.py
+++ get_prototype_target.py
@@ -1,11 +1,15 @@
 import torch
 from sklearn.cluster import KMeans
 from torch.utils.data import DataLoader
-from utils import cluster_acc
+from utils import cluster_acc, accuracy
 import dataset
 import argparse
 import os
 import torch.nn.functional as F
+
+import models
+import torch.nn as nn
+import torch.optim as optim
 
 device=torch.device('cuda' if torch.cuda.is_available else 'cpu')
 
@@ -63,8 +67,27 @@ for target in target_list:
     kmeans = KMeans(n_clusters=total_class_num, random_state=8, n_init="auto").fit(feature_target.cpu())
     pred_t = kmeans.labels_
 
-    centers = kmeans.cluster_centers_
-    torch.save(centers, f'./ckpt/prototype_target/{dataset_name}_{target}.pth')
-
     unseen_acc_D2 = cluster_acc(pred_t, label_t_all.numpy())
     print(f'Accuracy of {dataset_name} {target}: {unseen_acc_D2}')
+
+    # supervised learning using pseudo label get from K-means
+    model = models.MLP_simple(in_dim = 768, out_dim = total_class_num).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+    true_tmp = F.one_hot(torch.from_numpy(pred_t).to(torch.int64), num_classes=total_class_num).to(device)
+
+    feature_t = feature_target.to(torch.float32).to(device)
+    for ep in range(500):
+
+        model.train()
+        optimizer.zero_grad()
+
+        output = model(feature_t)
+
+        loss = criterion(output, true_tmp.float())
+
+        loss.backward()
+        optimizer.step()
+
+    torch.save(model.state_dict()['head.weight'].cpu().T.numpy(), f'./ckpt/prototype_target/{dataset_name}_{target}.pth')
